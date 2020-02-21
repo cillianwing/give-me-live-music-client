5591c2b94767b343359162abc26bddf1d79f2961
Cillian Wing <cwing@zagmail.gonzaga.edu>
2020-02-21 08:09:10 -0600

    Created currentUser.js reducer to take in currentUser.js actions and handle user signup, login, and logout.

---
+++ src/reducers/currentUser.js
@@ -0,0 +1,67 @@
+import { LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT_REQUEST, LOGOUT_SUCCESS } from '../actions/currentUser';
+
+const initialState = {
+  isLoading: false,
+  isAuthenticated: localStorage.getItem('token') ? true : false
+}
+
+const auth = (state = initialState, action) => {
+  switch (action.type) {
+    case SIGNUP_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+        isAuthenticated: false,
+        user: action.credentials
+      }
+    case SIGNUP_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        isAuthenticated: true,
+        errorMessage: ''
+      }
+    case SIGNUP_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        isAuthenticated: false,
+        errorMessage: action.message
+      }
+    case LOGIN_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+        isAuthenticated: false,
+        user: action.credentials
+      }
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        isAuthenticated: true,
+        errorMessage: ''
+      }
+    case LOGIN_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        isAuthenticated: false,
+        errorMessage: action.message
+      }
+    case LOGOUT_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+        isAuthenticated: true
+      }
+    case LOGOUT_SUCCESS:
+      return {
+        ...state,
+        isLoading:false,
+        isAuthenticated: false
+      }
+    default:
+      return state
+  }
+}
