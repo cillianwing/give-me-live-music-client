78bfa1e25d711d0f272fcb6e43af236b5cff81d3
Cillian Wing <cwing@zagmail.gonzaga.edu>
2020-02-26 20:15:52 -0600

    Updated Concert component to render necessary concert info on each card as well as 2 buttons, 1 to access more info and 1 to add the selected concert to user's concerts (interact with backend). Full onClick functions still need to be setup. Also updated UpcomingConcerts container so that all necessary concert parameters are passed as props to the Concert component called within the container.

--- src/components/Concert.js
+++ src/components/Concert.js
@@ -1,27 +1,30 @@
 import React from 'react'
-import { Container, Card, Row, Col, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Card, Row, Col } from 'react-bootstrap';
 import BasicButton from './input/BasicButton'
 
 const Concert = (props) => {
+
+  const handleGoingClick = (event, props) => {
+    event.preventDefault()
+  }
+  
+  const handleInfoClick = (event) => {
+    event.preventDefault()
+  }
+
   return (
-    
       <Card className="text-center my-2">
         <Card.Body>
           <Row>
-            <Col xs={12} md={8}>
+            <Col xs={12} md={9}>
               <Card.Title>{props.display}</Card.Title>
-              <Card.Text className="mb-0">
-                <strong>Venue: </strong>{props.venueName} in {props.location}
-              </Card.Text>
-              <Card.Text>
-                <strong>Date: </strong>{props.date} @ {props.time}
+              <Card.Text className="mb-1">
+                <strong>Venue: </strong>{props.venueName} in {props.location} | <strong>Date: </strong>{props.date} @ {props.time}
               </Card.Text>
             </Col>
-            <Col xs={12} md={4}>
-              <BasicButton size="sm" className="mr-1" style={{ width: "40%" }} variant="warning" value="Maybe!" />
-              <BasicButton size="sm" className="ml-1" style={{ width: "40%" }} variant="success" value="Going!" />
-              <BasicButton size="sm" block="block" variant="info" value="More Info" />
+            <Col xs={12} md={3}>
+              <BasicButton handleClick={event => handleGoingClick(event, props)} size="sm" className="mb-1" block="block" variant="success" value="Going!" />
+              <BasicButton handleClick={event => handleInfoClick(event, props)} size="sm" className="mt-1" block="block" variant="info" value="More Info" />
             </Col>
           </Row>
         </Card.Body>
--- src/containers/UpcomingConcerts.js
+++ src/containers/UpcomingConcerts.js
@@ -31,13 +31,39 @@ const UpcomingConcerts = (props) => {
     props.logoutUser()
   }
 
+  const headliners = (concert) => {
+    const headlinerObj = concert.performance.filter(performer => performer.billing === 'headline')
+    const headlinerArr = headlinerObj.map(artist => artist.displayName)
+    return headlinerArr
+  }
+
+  const support = (concert) => {
+    const supportObj = concert.performance.filter(performer => performer.billing === 'support')
+    const supportArr = supportObj.map(artist => artist.displayName)
+    return supportArr
+  }
+
+  const city = (concert) => {
+    return concert.location.city.split(', ')[0]
+  }
+
+  const region = (concert) => {
+    return concert.location.city.split(', ')[1]
+  }
+
   const concertCards = props.concerts.map(concert => (
       <Concert key={concert.id}
+        type={concert.type}
         display={concert.displayName} 
         date={concert.start.date} 
         time={concert.start.time} 
+        venueId={concert.venue.id}
         venueName={concert.venue.displayName}
+        headline={headliners(concert)}
+        support={support(concert)}
         location={concert.location.city}
+        city={city(concert)}
+        region={region(concert)}
         url={concert.uri}
         />
       )
