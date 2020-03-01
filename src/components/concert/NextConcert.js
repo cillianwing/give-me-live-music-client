119d3b0b1f8d3286e15f9baf36ac10b61bbaddf9
Cillian Wing <cwing@zagmail.gonzaga.edu>
2020-03-01 16:36:59 -0600

    Added some basic info to venue details section of NextConcert component from props passed by parent component UserConcerts.

--- src/components/concert/NextConcert.js
+++ src/components/concert/NextConcert.js
@@ -32,10 +32,9 @@ const NextConcert = (props) => {
         <Row className="justify-content-center">
           <Col className="text-center" style={{borderRight: "1px dashed blue"}} xs={6} sm={6} md={6} lg={6}>
             <Card.Body>
-            <Card.Title>{props.concert.type} Details</Card.Title>
+            <Card.Title className="mb-0">{params.headline.length !== 0 ? `Featuring: ${params.headline.map(artist => artist.displayName).join(' / ')}` : ''}</Card.Title>
               <Card.Text>
-                {params.headline.length !== 0 ? `Headline: ${params.headline.map(artist => artist.displayName).join(' / ')}` : ''}
-                {params.support.length !== 0 ? `  -  Support: ${params.support.map(artist => artist.displayName).join(' / ')}` : ''}
+                <strong>{params.support.length !== 0 ? `w/ special guests: ${params.support.map(artist => artist.displayName).join(' / ')}` : ''}</strong>
               </Card.Text>
               <Card.Text>
                 {params.date ? `Event starts on ${params.date.toLocaleDateString()}` : ''}{params.time ? ` @ ${params.time}` : ''}
@@ -48,9 +47,18 @@ const NextConcert = (props) => {
           </Col>
           <Col className="text-center" xs={6} sm={6} md={6} lg={6}>
             <Card.Body>
-            <Card.Title>Venue Details</Card.Title>
+            <Card.Title className="mb-0">{params.venueName}</Card.Title>
               <Card.Text>
-                Loading next concert venue details.
+                <strong>{params.venueDescription ? params.venueDescription : ''}</strong>
+              </Card.Text>
+              <Card.Text>
+                {params.street ? `${params.street} - ` : ''}{params.location}
+              </Card.Text>
+              <Card.Text>
+                {params.ageRestriction ? `Age Restriction: ${params.ageRestriction}` : 'Age Restriction: None'}
+              </Card.Text>
+              <Card.Text>
+                {params.venueCapacity ? `Venue Capacity: ${params.venueCapacity}` : 'See additional venue info by clicking link below'}
               </Card.Text>
             </Card.Body>
           </Col>
